9b7390122ef913e4e4b6b81a47a39fcf4ef17bb8
tjovanna <teeganjansen@gmail.com>
2016-05-04 15:40:44 -0400

    Grab user's long&lat and store in variable, load data from json file

--- 14 production/js/google_map.js
+++ 14 production/js/google_map.js
@@ -4,7 +4,10 @@
 
 //Make map & center on Davis Square
 var map;
+var geocoder;
+
 function initMap() {
+    geocoder = new google.maps.Geocoder();
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 42.396379, lng: -71.122309},
         zoom: 16
@@ -49,29 +52,57 @@ function initMap() {
 //        radius: 805
 //    });
 //
-////Turn user input (address) into LatLng
+// On click, call function to geocode user's address and load database results (no filters)
 //    var geocoder = new google.maps.Geocoder();
-//
-//    document.getElementById('search').addEventListener('click', function () {
-//        event.preventDefault();
-//        geocodeAddress(geocoder, map);
-//    });
-//}
-//function geocodeAddress() {
-//    var address = document.getElementById("address").value;
-//    geocoder.geocode({'address': address}, function (results, status) {
-//        if (status == google.maps.GeocoderStatus.OK) {
-//            map.setCenter(results[0].geometry.location);
-//            var marker = new google.maps.Marker({
-//                map: map,
-//                position: results[0].geometry.location
-//            });
-//        } else {
-//            alert("Geocode was not successful for the following reason: " + status);
-//        }
-//    });
+
+    document.getElementById('search').addEventListener('click', function (event) {
+        event.preventDefault();
+        geocodeAddress(geocoder, map);
+
+        //load results from json file
+        var xhr = new XMLHttpRequest();
+        responseObject = JSON.parse(xhr.responseText);
+        var newContent = '';
+        for(var i=0; i < responseObject.bathrooms.length; i++){
+            newContent += '<div class="result">';
+            newContent += '<h2>'+responseObject.bathrooms[i].name+ '</h2>';
+            newContent += '<h3>'+ responseObject.bathrooms[i].address+ '<br>';
+            newContent += responseObject.bathrooms[i].hours + '</h3>';
+            newContent += '<p>' +responseObject.bathrooms[i].description +'</p>';
+            newContent += '</div>';
+        }
+        document.getElementById('content').innerHTML = newContent;
+        xhr.open('GET', '../bathrooms.json', true);
+        xhr.send(null);
+    });
 }
-//Submit button doesn't go anywhere
-//document.getElementById("search").addEventListener("click", function (event) {
-//    event.preventDefault()
-//});
+
+//Grab user's longitude and latitude and place a 'you are here' marker
+var locLat;
+var locLng;
+
+function geocodeAddress() {
+    //var locLat;
+    //var locLng;
+    var address = document.getElementById("searchTextField").value;
+    geocoder.geocode({'address': address}, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            }
+            );
+            //Gets user location latitude and longitude
+            locLat = results[0].geometry.location.lat();
+            locLng = results[0].geometry.location.lng();
+            console.log(locLat);
+            console.log(locLng);
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
+}
+
+//Get distance between user location and database results
+
